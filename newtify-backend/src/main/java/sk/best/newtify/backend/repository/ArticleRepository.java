dc2f27d6d3737c692abffae5783604103860f0f8
Alejandro <100450996@alumnos.uc3m.es>
2022-07-15 09:27:48 +0200

    Pagination

--- newtify-backend/src/main/java/sk/best/newtify/backend/repository/ArticleRepository.java
+++ newtify-backend/src/main/java/sk/best/newtify/backend/repository/ArticleRepository.java
@@ -1,12 +1,12 @@
 package sk.best.newtify.backend.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import sk.best.newtify.backend.entity.Article;
 import sk.best.newtify.backend.entity.enums.TopicType;
 
 import java.util.List;
 
-public interface ArticleRepository extends CrudRepository<Article, String> {
+public interface ArticleRepository extends PagingAndSortingRepository<Article, String> {
 
     List<Article> findAllByOrderByCreatedAt();
 
